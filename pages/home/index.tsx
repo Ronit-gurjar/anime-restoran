4ddf5cd433d59ba95952783071093d5c9fb33303
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-07-09 20:20:20 +0530

    Add images to Home component

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import Image from 'next/image';
+import HeroRamen from '../../public/images/hero-ramen.png'
+import HeroStanding1 from '../../public/images/luffy-standing.png'
+import HeroStanding2 from '../../public/images/naruto-standing.png'
 
 const Home = () => {
   return (
@@ -8,13 +12,13 @@ const Home = () => {
       </div>
       <div className="home__bottom">
         <div className="home__image">
-          <img src="/image1.jpg" alt="Image 1" />
+          <Image src={HeroStanding1} alt="hero-image luffy standing" />
         </div>
         <div className="home__image">
-          <img src="/image2.jpg" alt="Image 2" />
+          <Image src={HeroRamen} alt="Ramen" />
         </div>
         <div className="home__image">
-          <img src="/image3.jpg" alt="Image 3" />
+          <Image src={HeroStanding2} alt="hero-image naruto standing" />
         </div>
       </div>
     </div>
