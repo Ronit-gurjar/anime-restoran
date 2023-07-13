7325dd1c3659393b9bfb9bfbfa105ab7a45d032b
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-07-13 23:13:30 +0530

    Create homeMenu component

---
+++ src/components/homeMenu/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomeMenu = () => {
+  return (
+    <div className="home-menu">
+      <div className="home-menu__top"></div>
+      <div className="home-menu__bottum"></div>
+    </div>
+  );
+};
+
+export default HomeMenu;
