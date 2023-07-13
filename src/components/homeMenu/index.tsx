2f979148c5d9d597cb0666ef578ea9decea45675
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-07-13 23:27:47 +0530

    Add title element to the homeMenu component

--- src/components/homeMenu/index.tsx
+++ src/components/homeMenu/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import {Poppins} from 'next/font/google'
+
+const poppins = Poppins({
+  weight: ['200', '400'],
+  style: ['normal', 'normal'],
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 const HomeMenu = () => {
   return (
     <div className="home-menu">
-      <div className="home-menu__top"></div>
+      <div className="home-menu__top">
+        <div className="home-menu__top-title"><h1 className={poppins.className}>Our Menu</h1></div>
+      </div>
+        
       <div className="home-menu__bottum"></div>
     </div>
   );
