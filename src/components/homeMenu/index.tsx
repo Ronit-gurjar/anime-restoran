3f08b861a6615cebe89cf1a6bb234f3394b21ce2
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-07-14 11:39:52 +0530

    Add banner images to the homeMenu component

--- src/components/homeMenu/index.tsx
+++ src/components/homeMenu/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import {Poppins} from 'next/font/google'
+import Image from 'next/image';
+import naruImg from '../../../public/images/naruto eating snacks.png';
+import sasuImg from '../../../public/images/sasuke eating snack.png';
+import FoodTypoImg from '../../../public/images/Foods.png';
+import DrinkTypoImg from '../../../public/images/Drinks.png';
 
 const poppins = Poppins({
   weight: ['200', '400'],
@@ -12,10 +17,17 @@ const HomeMenu = () => {
   return (
     <div className="home-menu">
       <div className="home-menu__top">
-        <div className="home-menu__top-title"><h1 className={poppins.className}>Our Menu</h1></div>
+        <div className="home-menu__top-title">
+            <Image src={naruImg} alt="naruto eating snack" />
+            <h1 className={poppins.className}>Our Menu</h1>
+            <Image src={sasuImg} alt="sasuke eating snack" />
+        </div>
+        <Image src={FoodTypoImg} alt="Foods" />
       </div>
         
-      <div className="home-menu__bottum"></div>
+      <div className="home-menu__bottum">
+        <Image src={DrinkTypoImg} alt="Drinks" />
+      </div>
     </div>
   );
 };
