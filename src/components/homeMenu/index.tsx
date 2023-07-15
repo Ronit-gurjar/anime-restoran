6966fc45dcb6c77a7c2dea9712b79d9638c11cd5
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-07-15 20:58:59 +0530

    Add embla carousel to food and drink div

--- src/components/homeMenu/index.tsx
+++ src/components/homeMenu/index.tsx
@@ -18,7 +18,8 @@ const poppins = Poppins({
 
 const HomeMenu = () => {
 
-    const [emblaRef] = useEmblaCarousel({ loop: false }, [Autoplay()])
+    const [emblaRef1] = useEmblaCarousel({ loop: false }, [Autoplay()])
+    const [emblaRef2] = useEmblaCarousel({ loop: false }, [Autoplay()])
 
   return (
     <div className="home-menu">
@@ -30,7 +31,7 @@ const HomeMenu = () => {
         </div>
         <div className="home-menu__top-content">
             <Image src={FoodTypoImg} alt="Foods" />
-            <div className="embla" ref={emblaRef}>
+            <div className="embla" ref={emblaRef1}>
                 <div className="embla__container">
                     <div className="embla__slide"><h1>Slide 1</h1></div>
                     <div className="embla__slide"><h1>Slide 1</h1></div>
@@ -43,7 +44,7 @@ const HomeMenu = () => {
       <div className="home-menu__bottum">
         <div className="home-menu__bottum-content">
             <Image src={DrinkTypoImg} alt="Drinks" />
-            <div className="embla" ref={emblaRef}>
+            <div className="embla" ref={emblaRef2}>
                 <div className="embla__container">
                     <div className="embla__slide"><h1>Slide 2</h1></div>
                     <div className="embla__slide"><h1>Slide 2</h1></div>
