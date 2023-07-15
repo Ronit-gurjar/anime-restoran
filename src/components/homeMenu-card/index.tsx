96d11c5495dd7c6a9147117acb414355408cfad9
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-07-16 00:13:10 +0530

    Create card component

---
+++ src/components/homeMenu-card/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Image from 'next/image'
+
+interface CardProps {
+  image: string;
+  title: string;
+  price: number;
+  rating: number;
+}
+
+const Card: React.FC<CardProps> = ({ image, title, price, rating }) => {
+  return (
+    <div className="card">
+      <Image src={image} alt={title} className="card__image" />
+      <h2 className="card__title">{title}</h2>
+      <p className="card__price">${price}</p>
+      <div className="card__rating">
+        {Array(rating)
+          .fill(null)
+          .map((_, index) => (
+            <span key={index} className="card__rating-star"></span>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Card;
