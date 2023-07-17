5c87e3ca877eadc8796b2857fa606c9c6f3f06fe
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-07-17 23:22:13 +0530

    fix: carousel for drink

--- src/components/embla-carousel-drink/index.tsx
+++ src/components/embla-carousel-drink/index.tsx
@@ -2,30 +2,10 @@ import React from 'react'
 import useEmblaCarousel from 'embla-carousel-react'
 import Autoplay from 'embla-carousel-autoplay'
 import Image from 'next/image'
+import drink1 from "./images/drink1.jpg";
+import drink2 from "./images/drink2.jpg"
 
-interface CardProps {
-  image: string;
-  title: string;
-  price: number;
-  rating: number;
-}
 
-const Card: React.FC<CardProps> = ({ image, title, price, rating }) => {
-  return (
-    <div className="card">
-      <Image src={image} alt={title} className="card__image" width={300} height={200}/>
-      <h2 className="card__title">{title}</h2>
-      <p className="card__price">${price}</p>
-      <div className="card__rating">
-        {Array(rating)
-          .fill(null)
-          .map((_, index) => (
-            <span key={index} className="card__rating-star"></span>
-          ))}
-      </div>
-    </div>
-  );
-};
 
 const Carousel_drink = () => {
     const [emblaRef1] = useEmblaCarousel({ loop: false }, [Autoplay()])
@@ -36,68 +16,44 @@ const Carousel_drink = () => {
         <div className="embla__container">
             <div className="embla__slide">
                 <div className="embla__slide-list">
-                <Card
-                image="/path/to/image.jpg"
-                title="Product Title"
-                price={19.99}
-                rating={4}
-                />
-                <Card
-                image="/path/to/image.jpg"
-                title="Product Title"
-                price={19.99}
-                rating={4}
-                />
-                <Card
-                image="/path/to/image.jpg"
-                title="Product Title"
-                price={19.99}
-                rating={4}
-                />
+                <div className="card">
+                  <Image src={drink1} alt="sweet lemon tea" className="card__image" width={200} height={200}/>
+                  <h2 className="card__title">"sweet lemon tea"</h2>
+                  <p className="card__price">$5</p>
+                </div>
+                <div className="card">
+                  <Image src={drink2} alt="latte coffee" className="card__image" width={200} height={200}/>
+                  <h2 className="card__title">"latte coffee"</h2>
+                  <p className="card__price">$21</p>
+                </div>
             </div>
             </div>
             <div className="embla__slide">
                 <div className="embla__slide-list">
-                <Card
-                image="/path/to/image.jpg"
-                title="Product Title"
-                price={19.99}
-                rating={4}
-                />
-                <Card
-                image="/path/to/image.jpg"
-                title="Product Title"
-                price={19.99}
-                rating={4}
-                />
-                <Card
-                image="/path/to/image.jpg"
-                title="Product Title"
-                price={19.99}
-                rating={4}
-                />
+                <div className="card">
+                  <Image src={drink1} alt="sweet lemon tea" className="card__image" width={200} height={200}/>
+                  <h2 className="card__title">"sweet lemon tea"</h2>
+                  <p className="card__price">$5</p>
+                </div>
+                <div className="card">
+                  <Image src={drink2} alt="latte coffee" className="card__image" width={200} height={200}/>
+                  <h2 className="card__title">"latte coffee"</h2>
+                  <p className="card__price">$21</p>
+                </div>
             </div>
             </div>
             <div className="embla__slide">
                 <div className="embla__slide-list">
-                <Card
-                image="/path/to/image.jpg"
-                title="Product Title"
-                price={19.99}
-                rating={4}
-                />
-                <Card
-                image="/path/to/image.jpg"
-                title="Product Title"
-                price={19.99}
-                rating={4}
-                />
-                <Card
-                image="/path/to/image.jpg"
-                title="Product Title"
-                price={19.99}
-                rating={4}
-                />
+                <div className="card">
+                  <Image src={drink1} alt="sweet lemon tea" className="card__image" width={200} height={200}/>
+                  <h2 className="card__title">"sweet lemon tea"</h2>
+                  <p className="card__price">$5</p>
+                </div>
+                <div className="card">
+                  <Image src={drink2} alt="latte coffee" className="card__image" width={200} height={200}/>
+                  <h2 className="card__title">"latte coffee"</h2>
+                  <p className="card__price">$21</p>
+                </div>
             </div>
             </div>
         </div>
